64cd52a70618521255a7e3f9ceb7615581891cf4
Manuel Krebber <admin@wheerd.de>
2017-01-12 14:30:20 +0100

    Fixed options for build command not being inherited.

--- distutils_build_without_typehints/build_without_typehints.py
+++ distutils_build_without_typehints/build_without_typehints.py
@@ -1,35 +1,29 @@
-import os.path
 import sys
 from distutils.command.build_py import build_py as _build_py
-from distutils.core import Command
+from distutils.command.build import build as _build
 
 from .strip_type_hints import StripTypeHintsRefactoringTool
 
 
-class build_without_typehints(Command):
-
+class build_without_typehints(_build):
     description = 'build without typehints'
-
-    user_options = []
-
-    boolean_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
 
     def run(self):
         if sys.version_info < (3, 5, 3):
             self.distribution.cmdclass['build_py'] = build_py
             self.reinitialize_command('build_py')
-        self.run_command('build')
+        _build.run(self)
 
 class build_py(_build_py):
     def initialize_options(self):
         _build_py.initialize_options(self)
         self.refactoring_tool = StripTypeHintsRefactoringTool()
+
+    def finalize_options(self):
+        self.set_undefined_options('build_without_typehints',
+                                   ('build_lib', 'build_lib'),
+                                   ('force', 'force'))
+        _build_py.finalize_options(self)
 
     def build_module(self, module, module_file, package):
         if not _build_py.build_module(self, module, module_file, package):
